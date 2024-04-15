cfa106a141070a0abb0da47ba944e8f701a94410
Resicadiuz <125767008+Resicadiuz@users.noreply.github.com>
2024-04-14 22:44:29 -0600

    Update MantenimientoTipoProductoDAL.cs
    Restaure ya que no se corrigio el error que trataba de corregir

--- SistemaVerduleria/DAL/MantenimientoTipoProductoDAL.cs
+++ SistemaVerduleria/DAL/MantenimientoTipoProductoDAL.cs
@@ -1,55 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 
 namespace DAL
 {
+    //Capa de acceso a datos DAL
     public class MantenimientoTipoProductoDAL
     {
-        private readonly DataBaseDAL db;
-
-        public MantenimientoTipoProductoDAL()
-        {
-            db = new DataBaseDAL();
-        }
-
         public void InsertaTipoProducto(string nombre, string tipoProducto, string tipoPrecio, int cantidad, decimal precio)
         {
-            SqlCommand comando = CrearComando("SP_RegistraTipoProducto");
-            AgregarParametros(comando, nombre, tipoProducto, tipoPrecio, cantidad, precio);
-            db.InsercionDatos(comando);
-        }
 
-        public void ActualizaTipoProducto(string nombre, string tipoProducto, string tipoPrecio, int cantidad, decimal precio)
-        {
-            SqlCommand comando = CrearComando("SP_ActualizaTipoProducto");
-            AgregarParametros(comando, nombre, tipoProducto, tipoPrecio, cantidad, precio);
-            db.InsercionDatos(comando);
-        }
-
-        public bool ValidaExistenciaProducto(string nombre)
-        {
-            SqlCommand comando = CrearComando("SP_ConsultaExistenciaProducto");
-            comando.Parameters.AddWithValue("@Nombre", nombre);
-
-            DataSet ds = db.LecturaDatos(comando, "[T_Productos]");
-
-            return (ds.Tables[0].Rows.Count > 0);
-        }
-
-        private SqlCommand CrearComando(string storedProcedure)
-        {
-            SqlCommand comando = new SqlCommand(storedProcedure);
+            DataBaseDAL db = new DataBaseDAL();
+            SqlCommand comando = new SqlCommand("SP_RegistraTipoProducto");
             comando.CommandType = CommandType.StoredProcedure;
-            return comando;
-        }
 
-        private void AgregarParametros(SqlCommand comando, string nombre, string tipoProducto, string tipoPrecio, int cantidad, decimal precio)
-        {
             comando.Parameters.AddWithValue("@Nombre", nombre);
             comando.Parameters.AddWithValue("@TipoProducto", tipoProducto);
             comando.Parameters.AddWithValue("@TipoPrecio", tipoPrecio);
             comando.Parameters.AddWithValue("@Cantidad", cantidad);
             comando.Parameters.AddWithValue("@Precio", precio);
+
+            db.InsercionDatos(comando);
+        }
+
+        public void ActualizaTipoProducto(string nombre, string tipoProducto, string tipoPrecio, int cantidad, decimal precio)
+        {
+
+            DataBaseDAL db = new DataBaseDAL();
+            SqlCommand comando = new SqlCommand("SP_ActualizaTipoProducto");
+            comando.CommandType = CommandType.StoredProcedure;
+
+            comando.Parameters.AddWithValue("@Nombre", nombre);
+            comando.Parameters.AddWithValue("@TipoProducto", tipoProducto);
+            comando.Parameters.AddWithValue("@TipoPrecio", tipoPrecio);
+            comando.Parameters.AddWithValue("@Cantidad", cantidad);
+            comando.Parameters.AddWithValue("@Precio", precio);
+
+            db.InsercionDatos(comando);
+        }
+
+        public bool ValidaExistenciaProducto(string nombre)
+        {
+            bool existe;
+
+            DataBaseDAL db = new DataBaseDAL();
+            SqlCommand comando = new SqlCommand("SP_ConsultaExistenciaProducto");
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@Nombre", nombre);
+
+            DataSet ds = db.LecturaDatos(comando, "[T_Productos]");
+
+            if (ds.Tables[0].Rows.Count == 0)
+            {
+                existe = false;
+            }
+            else
+            {
+                existe = true;
+            }
+
+            return existe;
         }
     }
 }
+
