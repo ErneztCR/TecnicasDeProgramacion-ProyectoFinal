0f976cbcfc7e6311270f9f44e2d01243c264d4ea
Resicadiuz <125767008+Resicadiuz@users.noreply.github.com>
2024-04-14 22:31:50 -0600

    Clean code MantenimientoTipoProductoDAL.cs
    Constructor: He eliminado la inicialización repetitiva de DataBaseDAL en cada método y lo he movido al constructor para seguir el principio DRY (Don't Repeat Yourself).
    Métodos de operación de base de datos: Simplifiqué los métodos InsertaTipoProducto, ActualizaTipoProducto y ValidaExistenciaProducto extrayendo la creación de comandos y la adición de parámetros a métodos auxiliares privados para mejorar la legibilidad y mantenibilidad del código.
    Uso de expresiones ternarias: En el método ValidaExistenciaProducto, reemplacé la estructura if-else con una expresión ternaria más concisa. Esto hace que el código sea más claro y compacto.

--- SistemaVerduleria/DAL/MantenimientoTipoProductoDAL.cs
+++ SistemaVerduleria/DAL/MantenimientoTipoProductoDAL.cs
@@ -1,69 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 
 namespace DAL
 {
-    //Capa de acceso a datos DAL
     public class MantenimientoTipoProductoDAL
     {
+        private readonly DataBaseDAL db;
+
+        public MantenimientoTipoProductoDAL()
+        {
+            db = new DataBaseDAL();
+        }
+
         public void InsertaTipoProducto(string nombre, string tipoProducto, string tipoPrecio, int cantidad, decimal precio)
         {
-
-            DataBaseDAL db = new DataBaseDAL();
-            SqlCommand comando = new SqlCommand("SP_RegistraTipoProducto");
-            comando.CommandType = CommandType.StoredProcedure;
-
-            comando.Parameters.AddWithValue("@Nombre", nombre);
-            comando.Parameters.AddWithValue("@TipoProducto", tipoProducto);
-            comando.Parameters.AddWithValue("@TipoPrecio", tipoPrecio);
-            comando.Parameters.AddWithValue("@Cantidad", cantidad);
-            comando.Parameters.AddWithValue("@Precio", precio);
-
+            SqlCommand comando = CrearComando("SP_RegistraTipoProducto");
+            AgregarParametros(comando, nombre, tipoProducto, tipoPrecio, cantidad, precio);
             db.InsercionDatos(comando);
         }
 
         public void ActualizaTipoProducto(string nombre, string tipoProducto, string tipoPrecio, int cantidad, decimal precio)
         {
-
-            DataBaseDAL db = new DataBaseDAL();
-            SqlCommand comando = new SqlCommand("SP_ActualizaTipoProducto");
-            comando.CommandType = CommandType.StoredProcedure;
-
-            comando.Parameters.AddWithValue("@Nombre", nombre);
-            comando.Parameters.AddWithValue("@TipoProducto", tipoProducto);
-            comando.Parameters.AddWithValue("@TipoPrecio", tipoPrecio);
-            comando.Parameters.AddWithValue("@Cantidad", cantidad);
-            comando.Parameters.AddWithValue("@Precio", precio);
-
+            SqlCommand comando = CrearComando("SP_ActualizaTipoProducto");
+            AgregarParametros(comando, nombre, tipoProducto, tipoPrecio, cantidad, precio);
             db.InsercionDatos(comando);
         }
 
         public bool ValidaExistenciaProducto(string nombre)
         {
-            bool existe;
-
-            DataBaseDAL db = new DataBaseDAL();
-            SqlCommand comando = new SqlCommand("SP_ConsultaExistenciaProducto");
-            comando.CommandType = CommandType.StoredProcedure;
+            SqlCommand comando = CrearComando("SP_ConsultaExistenciaProducto");
             comando.Parameters.AddWithValue("@Nombre", nombre);
 
             DataSet ds = db.LecturaDatos(comando, "[T_Productos]");
 
-            if (ds.Tables[0].Rows.Count == 0)
-            {
-                existe = false;
-            }
-            else
-            {
-                existe = true;
-            }
+            return (ds.Tables[0].Rows.Count > 0);
+        }
 
-            return existe;
+        private SqlCommand CrearComando(string storedProcedure)
+        {
+            SqlCommand comando = new SqlCommand(storedProcedure);
+            comando.CommandType = CommandType.StoredProcedure;
+            return comando;
+        }
+
+        private void AgregarParametros(SqlCommand comando, string nombre, string tipoProducto, string tipoPrecio, int cantidad, decimal precio)
+        {
+            comando.Parameters.AddWithValue("@Nombre", nombre);
+            comando.Parameters.AddWithValue("@TipoProducto", tipoProducto);
+            comando.Parameters.AddWithValue("@TipoPrecio", tipoPrecio);
+            comando.Parameters.AddWithValue("@Cantidad", cantidad);
+            comando.Parameters.AddWithValue("@Precio", precio);
         }
     }
 }
